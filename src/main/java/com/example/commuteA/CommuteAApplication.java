912a561a777a734289ea90f260b07015f2c61fb2
SIJIN9090 <dol9090@naver.com>
2025-03-20 09:08:18 +0900

    a

---
+++ src/main/java/com/example/commuteA/CommuteAApplication.java
@@ -0,0 +1,13 @@
+package com.example.commuteA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuteAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuteAApplication.class, args);
+	}
+
+}
